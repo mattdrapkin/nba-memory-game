4979018976fdecfa906b6eae0029d8621e12587e
Matt Drapkin <matt@drapkin.org>
2022-05-27 14:52:20 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.b1f8f0858aff9795b56f80e932696a31.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "35a1cca7bc5d7384c581",
+    "url": "/nba-memory-game/static/css/main.9c57d3c3.chunk.css"
+  },
+  {
+    "revision": "35a1cca7bc5d7384c581",
+    "url": "/nba-memory-game/static/js/main.35a1cca7.chunk.js"
+  },
+  {
+    "revision": "f138365e9f720a1bfa78",
+    "url": "/nba-memory-game/static/js/1.f138365e.chunk.js"
+  },
+  {
+    "revision": "670aaeb42f681b0be310",
+    "url": "/nba-memory-game/static/js/runtime~main.670aaeb4.js"
+  },
+  {
+    "revision": "cf8f3aaefd700e5ecfe8eb8a5e7c120f",
+    "url": "/nba-memory-game/static/media/thumbs-up.cf8f3aae.jpeg"
+  },
+  {
+    "revision": "e69a6812dc00537ac204b0e47e507f01",
+    "url": "/nba-memory-game/static/media/nba-logo.e69a6812.png"
+  },
+  {
+    "revision": "c9d8936499de800d60b821ccb7d8173f",
+    "url": "/nba-memory-game/static/media/bulls.c9d89364.png"
+  },
+  {
+    "revision": "f99636a45620c4b6b17d5c4cd108d8e3",
+    "url": "/nba-memory-game/static/media/cavs.f99636a4.png"
+  },
+  {
+    "revision": "4d980cdd2e12512a02f28fcabda056dd",
+    "url": "/nba-memory-game/static/media/celtics.4d980cdd.png"
+  },
+  {
+    "revision": "b285c665def8259a6e3603c64a22d84c",
+    "url": "/nba-memory-game/static/media/hawks.b285c665.png"
+  },
+  {
+    "revision": "972afb576492bcb7e5ea7330e3e3958e",
+    "url": "/nba-memory-game/static/media/heat.972afb57.png"
+  },
+  {
+    "revision": "3d1059890dbb02a6d470c3fdaff52cd0",
+    "url": "/nba-memory-game/static/media/hornets.3d105989.png"
+  },
+  {
+    "revision": "c61f9af2d6b2d14b42a49a97698c1be3",
+    "url": "/nba-memory-game/static/media/knicks.c61f9af2.png"
+  },
+  {
+    "revision": "c879409d670f7bfae17946b7b9be7feb",
+    "url": "/nba-memory-game/static/media/lakers.c879409d.png"
+  },
+  {
+    "revision": "78a3f61693eea7831e4401c51ff026e6",
+    "url": "/nba-memory-game/static/media/magic.78a3f616.png"
+  },
+  {
+    "revision": "e449a38ed255f37bba213a4d6c9e5381",
+    "url": "/nba-memory-game/static/media/mavs.e449a38e.png"
+  },
+  {
+    "revision": "68d8c38aa73a20259ff6e5e77869e919",
+    "url": "/nba-memory-game/static/media/sixers.68d8c38a.png"
+  },
+  {
+    "revision": "50e56b6cd9a82e9a9031b1fad4c66304",
+    "url": "/nba-memory-game/static/media/suns.50e56b6c.png"
+  },
+  {
+    "revision": "2bf73415ea741259f307f8e6dbc45f3f",
+    "url": "/nba-memory-game/index.html"
+  }
+];
